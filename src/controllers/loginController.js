791373304e64f3addf9c78915c1bc79c5e701ee8
pedro henrique vieira <pedrovs3@hotmail.com>
2022-08-20 18:27:57 -0300

    Sistema de Login funcionando

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,14 +1,15 @@
 const Login = require('../models/loginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
-}
+  if(req.session.user) return res.render('logado')
+  return res.render('login');
+};
 
 exports.register = async (req, res) => {
-  try{
+  try {
     const login = new Login(req.body);
     await login.register();
-  
+
     if (login.errors.length > 0) {
       req.flash('errors', login.errors);
       req.session.save(() => {
@@ -21,8 +22,39 @@ exports.register = async (req, res) => {
     req.session.save(() => {
       return res.redirect('/login');
     });
-  } catch (e){
-    console.log(e)
-    return res.render('404')
+  } catch (e) {
+    console.log(e);
+    return res.render('404');
   }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(() => {
+        return res.redirect('./');
+      });
+      return;
+    }
+
+    req.flash('success', 'Logado com sucesso');
+    req.session.user = login.user;
+    req.session.save(() => {
+      return res.redirect('./');
+    });
+
+  } catch (e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.logout = (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/');
+  next();
 }
